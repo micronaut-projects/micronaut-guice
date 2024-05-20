eddfbeaaf2cdf183a5d3ccf9cee0aaefff5c49f8
Sergio del Amo <sergio.delamo@softamo.com>
2024-05-20 19:24:27 +0200

    fix test

--- micronaut-guice/src/test/java/io/micronaut/guice/doc/examples/bindings/order/ModuleOrderTest.java
+++ micronaut-guice/src/test/java/io/micronaut/guice/doc/examples/bindings/order/ModuleOrderTest.java
@@ -1,15 +1,15 @@
 package io.micronaut.guice.doc.examples.bindings.order;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import io.micronaut.guice.annotation.Guice;
+import io.micronaut.guice.doc.examples.bindings.defaultimplementation.GreeterModule;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(startApplication = false, environments = ModuleOrderTest.ENV)
 @Guice(modules = { One.class, Two.class }, environments = ModuleOrderTest.ENV)
@@ -21,8 +21,9 @@ class ModuleOrderTest {
     @Test
     void testOrder() {
         assertEquals(3, modules.size());
-        assertInstanceOf(One.class, modules.get(0));
-        assertInstanceOf(Two.class, modules.get(1));
+        assertTrue(modules.stream().anyMatch(m -> m instanceof One));
+        assertTrue(modules.stream().anyMatch(m -> m instanceof Two));
+        assertTrue(modules.stream().anyMatch(m -> m instanceof GreeterModule));
     }
 }
 
