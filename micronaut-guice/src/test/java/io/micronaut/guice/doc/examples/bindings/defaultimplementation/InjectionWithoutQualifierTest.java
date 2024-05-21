d2e19293b0a19ee7ac82b63b9ca0678a077d0288
Sergio del Amo <sergio.delamo@softamo.com>
2024-05-21 11:34:29 +0200

    test: remove disabled

--- micronaut-guice/src/test/java/io/micronaut/guice/doc/examples/bindings/defaultimplementation/InjectionWithoutQualifierTest.java
+++ micronaut-guice/src/test/java/io/micronaut/guice/doc/examples/bindings/defaultimplementation/InjectionWithoutQualifierTest.java
@@ -2,7 +2,6 @@ package io.micronaut.guice.doc.examples.bindings.defaultimplementation;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,7 +12,6 @@ class InjectionWithoutQualifierTest {
     @Inject
     Greeter greeter;
 
-    @Disabled("io.micronaut.context.exceptions.NonUniqueBeanException: Multiple possible bean candidates found: [Greeter, Greeter]")
     @Test
     void qualifiedBeanFromContextGetBean() {
         assertEquals("Hello", greeter.hello());
