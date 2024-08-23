b98d8ad2974add1dc692fa11159f0c5c34dcea77
sdelamo <sdelamo@users.noreply.github.com>
2024-08-23 09:39:22 +0000

    Deploying to gh-pages - 09:39:22

---
+++ 1.1.x/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.guice.annotation"},{"l":"io.micronaut.guice.annotation.internal"},{"l":"io.micronaut.guice.processor"}];updateSearchResults();
