0723a35c2704908230c5e6877f6d5fac58a778f8
Sergio del Amo <sergio.delamo@softamo.com>
2024-08-23 11:21:02 +0200

    enable binary compatibility (#23)
    * enable binary compatibility
    
    * Update io.micronaut.build.internal.guice-module.gradle

--- micronaut-guice-bom/build.gradle.kts
+++ micronaut-guice-bom/build.gradle.kts
@@ -2,9 +2,3 @@ plugins {
     id("io.micronaut.build.internal.guice-base")
     id("io.micronaut.build.internal.bom")
 }
-
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
