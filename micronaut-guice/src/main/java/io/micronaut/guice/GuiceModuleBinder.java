00c1ab1958b5f95ae656ad973780f53826980881
issam liqali <liqaliissam@gmail.com>
2025-10-16 10:26:58 +0100

    fix checkstyle

--- micronaut-guice/src/main/java/io/micronaut/guice/GuiceModuleBinder.java
+++ micronaut-guice/src/main/java/io/micronaut/guice/GuiceModuleBinder.java
@@ -358,7 +358,7 @@ class GuiceModuleBinder implements Binder {
         }
     }
 
-    private static class AnnotatedConstantBindingBuilderImpl implements AnnotatedConstantBindingBuilder, ConstantBindingBuilder {
+    private static final class AnnotatedConstantBindingBuilderImpl implements AnnotatedConstantBindingBuilder, ConstantBindingBuilder {
         private Object value;
         private Class<? extends Annotation> annotationType;
         private String name;
@@ -446,12 +446,12 @@ class GuiceModuleBinder implements Binder {
     }
 
     private class LinkedBindingBuilderImpl<T> implements LinkedBindingBuilder<T>, AnnotatedBindingBuilder<T> {
-        private final Argument<T> beanType;
 
         private static final List<Class<? extends Annotation>> SINGLETON_CLASSES = List.of(
                 Singleton.class,
                 jakarta.inject.Singleton.class
         );
+        private final Argument<T> beanType;
 
         private boolean isSingleton;
         private Class<? extends Annotation> scope;
