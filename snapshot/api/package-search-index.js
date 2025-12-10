9c4e68c6b54e7a4dc41d6364ff669ad49b22aae1
sdelamo <sdelamo@users.noreply.github.com>
2025-12-10 06:11:11 +0000

    Deploying to gh-pages - 06:11:11

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.guice.annotation"},{"l":"io.micronaut.guice.annotation.internal"},{"l":"io.micronaut.guice.processor"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.guice.annotation"},{"l":"io.micronaut.guice.annotation.internal"},{"l":"io.micronaut.guice.info"},{"l":"io.micronaut.guice.processor"}];updateSearchResults();
